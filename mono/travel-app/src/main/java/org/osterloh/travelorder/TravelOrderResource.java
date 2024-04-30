8d8f48eb1274e135d35339e305a3b19bf86139f7
Johnatan Osterloh <johnatanosterloh@gmail.com>
2024-04-30 16:18:03 -0300

    TravelOrder

--- mono/travel-app/src/main/java/org/osterloh/travelorder/TravelOrderResource.java
+++ mono/travel-app/src/main/java/org/osterloh/travelorder/TravelOrderResource.java
@@ -1,13 +1,24 @@
 package org.osterloh.travelorder;
 
+import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import org.osterloh.flight.Flight;
+import org.osterloh.flight.FlightResource;
+import org.osterloh.hotel.Hotel;
+import org.osterloh.hotel.HotelResource;
 
 import java.util.List;
 
 @Path("travelorder")
 public class TravelOrderResource {
+
+    @Inject
+    FlightResource flightResource;
+
+    @Inject
+    HotelResource hotelResource;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -25,9 +36,21 @@ public class TravelOrderResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public TravelOrder newTravelOrder(TravelOrder order){
+    public TravelOrder newTravelOrder(TravelOrderDTO orderDto){
+        TravelOrder order = new TravelOrder();
         order.id = null;
         order.persist();
+
+        Flight flight = new Flight();
+        flight.fromAirport = orderDto.getFromAirport();
+        flight.toAirport = orderDto.getToAirport();
+        flight.travelOrderId = order.id;
+        flightResource.newFlight(flight);
+
+        Hotel hotel = new Hotel();
+        hotel.nights = orderDto.getNights();
+        hotel.travelOrderId = order.id;
+        hotelResource.newHotel(hotel);
 
         return order;
     }
