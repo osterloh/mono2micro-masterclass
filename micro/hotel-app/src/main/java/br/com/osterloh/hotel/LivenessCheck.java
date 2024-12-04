259092182c0bc7438cb98dfba560272214e7d878
Johnatan Osterloh <johnatanosterloh@gmail.com>
2024-12-03 23:30:49 -0300

    Configuração incial do Liveness

---
+++ micro/hotel-app/src/main/java/br/com/osterloh/hotel/LivenessCheck.java
@@ -0,0 +1,14 @@
+package br.com.osterloh.hotel;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+public class LivenessCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("Hello world");
+    }
+}
